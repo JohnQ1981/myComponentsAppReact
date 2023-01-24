3a033bb09d43566b938c2425f91901544512a0f4
JohnQ1981 <ikram1981@gmail.com>
2023-01-23 21:23:14 -0600

    added props-types

--- mycomponents/src/App.js
+++ mycomponents/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       App-New App being created!
+      <div><Button>Click Me!</Button></div>
+      <div><Button>Buy Now!</Button></div>
+      <div><Button>See Deal!</Button></div>
+      <div><Button>Hide Ads!</Button></div>
     </div>
   );
 }
