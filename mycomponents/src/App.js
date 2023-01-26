a2c33d1797c6a0b82b8d11028c9443aade4c1279
JohnQ1981 <ikram1981@gmail.com>
2023-01-25 21:02:49 -0600

    button css is done

--- mycomponents/src/App.js
+++ mycomponents/src/App.js
@@ -1,14 +1,42 @@
-import './App.css';
-import Button from './components/Button';
+import "./App.css";
+import { DiApple } from "react-icons/di";
+import { CiBatteryFull } from "react-icons/ci";
+import { MdShoppingCart } from "react-icons/md";
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       App-New App being created!
-      <div><Button>Click Me!</Button></div>
-      <div><Button>Buy Now!</Button></div>
-      <div><Button>See Deal!</Button></div>
-      <div><Button>Hide Ads!</Button></div>
+      <div>
+        <Button success>
+          <DiApple />
+          Click Me!
+        </Button>
+      </div>
+      <div>
+        <Button success rounded outline>
+          <CiBatteryFull />
+          Charge Me!
+        </Button>
+      </div>
+      <div>
+        <Button danger>Click Me!</Button>
+      </div>
+      <div>
+        <Button warning><MdShoppingCart />Buy Now!</Button>
+      </div>
+      <div>
+        <Button outline danger>
+          <CiBatteryFull />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Hide Ads!
+        </Button>
+      </div>
     </div>
   );
 }
