19d2d990d8ad8d282f337fa1dc12c703722f6160
JohnQ1981 <ikram1981@gmail.com>
2023-02-21 20:45:36 -0600

    still need to work

--- mycomponents/src/components/Dropdown.js
+++ mycomponents/src/components/Dropdown.js
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {GoChevronDown} from 'react-icons/go';
 import Panel from "./Panel";
 
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+  useEffect (()=>{
+    const handler =(event)=>{
+      console.log(event.target);
+    };
+    document.addEventListener('click', handler, true)
+  },[])
 
   const handleOptionClick = (option) => {
     //Close Dropdown
