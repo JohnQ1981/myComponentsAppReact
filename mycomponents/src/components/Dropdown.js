cb2c0f43b35592c1119a37acdb57c322a6220aa2
JohnQ1981 <ikram1981@gmail.com>
2023-02-21 20:40:04 -0600

    added Pnel

--- mycomponents/src/components/Dropdown.js
+++ mycomponents/src/components/Dropdown.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import {GoChevronDown} from 'react-icons/go';
+import Panel from "./Panel";
 
 
 function Dropdown({ options, value, onChange }) {
@@ -35,14 +36,14 @@ function Dropdown({ options, value, onChange }) {
 
   return (
     <div className="w-48 relative">
-      <div
-        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+      <Panel
+        className="flex justify-between items-center cursor-pointer "
         onClick={handleClick}
       >
         {value?.label || "Select ..."}
         <GoChevronDown className="text-lg" />
-      </div>
-      {isOpen && <div className="absolute top-full order rounded p-3 shadow bg-white w-full"> {renderedOption}</div>}
+      </Panel>
+      {isOpen && <Panel className="absolute top-full "> {renderedOption}</Panel>}
     </div>
   );
 }
